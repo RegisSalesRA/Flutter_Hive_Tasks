cabf4fb51632bfd94573550bb20575c01ad3af32
RegisSalesRA <regismsales@gmail.com>
2021-11-01 21:12:04 -0300

    change atividades model

--- client/lib/model/atividade_model.dart
+++ client/lib/model/atividade_model.dart
@@ -6,8 +6,11 @@ class Atividades extends HiveObject {
   @HiveField(0)
   String? nome;
 
-  @HiveField(1)
+  @HiveField(1, defaultValue: false)
   bool? isComplete;
+
+  @HiveField(2,defaultValue: false)
+  bool? available;
 
   Atividades({this.nome, this.isComplete});
 }
